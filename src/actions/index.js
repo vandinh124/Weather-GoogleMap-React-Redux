85f2074e9d39261107ea7f2fb529cc4e301cae59
Van Dinh <48852572+vandinh124@users.noreply.github.com>
2020-09-13 19:22:26 -0700

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'f022b5919be8b000c63567cefd089c44';
+const API_KEY = 'API';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
